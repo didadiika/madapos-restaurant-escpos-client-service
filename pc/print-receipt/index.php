0f6321eab5a54a68f9f0fb7a54701c2176a2132f
Dika <didadiika@gmail.com>
2024-08-15 19:35:07 +0700

    Mengubah Print Receipt

--- pc/print-receipt/index.php
+++ pc/print-receipt/index.php
@@ -35,9 +35,12 @@ $jumlah_print = $_POST['jumlah_print'];
             $pr_usb[$nump] = $receipt->usb;
             $pr_ip[$nump] = $receipt->ip;
                 /**INISIALIASASI SETTING KERTAS DAN ALIGMENT**/
-                if(($receipt->paper == '58mm' && $receipt->type == '58') || ($receipt->paper == '80mm' && $receipt->type == '80'))
+                if($receipt->type == '58' || ($receipt->paper == '80mm' && $receipt->type == '80'))
                 {
-                    if($receipt->paper == '58mm'){$lebar_pixel = 32;}else {$lebar_pixel = 48; }
+                    $lebar_pixel = 48; 
+                    if($receipt->paper == '58mm'){
+                        $lebar_pixel = 32;
+                    } 
                     $center = 'On';
                     $right = 'On';
                 }
@@ -90,16 +93,9 @@ $jumlah_print = $_POST['jumlah_print'];
                     
                     /** JUMLAH PRINT **/
                     for($i= 0; $i < $jumlah_print; $i++ ){
-                        /** LOOPING MAKANAN **/
-                        // $logo = EscposImage::load($images_path."/".$data->store->photo);
-                        // if($center == 'On')
-                        // {
-                        // $printer -> setJustification(Printer::JUSTIFY_CENTER);
-                        // }
-                        // $printer->bitImage($logo);
-                        // $printer -> feed();
                    
-                    #Toko
+
+                    // Logo
                     if($center == 'On')
                     {
                         $printer -> setJustification(Printer::JUSTIFY_CENTER);
@@ -116,9 +112,15 @@ $jumlah_print = $_POST['jumlah_print'];
                     $printer->text($data->store->address."\n");
                     $printer->text($data->store->city."\n");
                     $printer->text($data->store->phone."\n");
+                    // Logo
+                    
+
 
                     $printer->selectPrintMode(Printer::MODE_FONT_A | Printer::MODE_DOUBLE_HEIGHT | Printer::MODE_DOUBLE_WIDTH);
-                    
+
+
+
+                    // Dine In / Take Away
                     if($data->customer->dine_type == "Dine In"){
                         $printer -> setTextSize(3, 2);
                         $printer -> text("#".$data->customer->numb_desk."\n");
@@ -127,67 +129,81 @@ $jumlah_print = $_POST['jumlah_print'];
                     } else {
                         $printer -> text("#".$data->customer->dine_type."\n");
                     }
-                    
                     $printer -> setTextSize(1, 1);
                     $printer->setEmphasis(true);//berguna mempertebal huruf
-                   
                     if($center == 'On')
                     {
                     $printer -> setJustification(Printer::JUSTIFY_CENTER);
                     }
                     $printer -> text(str_repeat('=',$lebar_pixel)."\n");
+                    // Dine In / Take Away
+                    
+                    
+                    // Header
                     $printer -> setBarcodeHeight(40);
                     $printer -> setBarcodeWidth(2);
                     $printer->barcode($data->customer->sale_uid, Printer::BARCODE_CODE39);
                     $printer -> setJustification(Printer::JUSTIFY_LEFT);
-                    $printer->text("UID      : ".$data->customer->sale_uid."\n");
-                    $printer->text("Pelanggan: ".$data->customer->customer_name."\n");
-                    $printer->text("Tanggal  : ".$data->customer->date."\n");
-                    $printer->text("Kasir    : ".$data->customer->cashier."\n");
+                    $printer->text("UID      : ".substr($data->customer->sale_uid,0,$lebar_pixel - 11)."\n");
+                    $printer->text("Pelanggan: ".substr($data->customer->customer_name,0,$lebar_pixel - 11)."\n");
+                    $printer->text("Tanggal  : ".substr($data->customer->date,0,$lebar_pixel - 11)."\n");
+                    $printer->text("Kasir    : ".substr($data->customer->cashier,0,$lebar_pixel - 11)."\n");
+                    // Header
+
+
+
+
+
+                // Items
                     if($center == 'On')
                     {
-                    $printer -> setJustification(Printer::JUSTIFY_CENTER);
+                        $printer -> setJustification(Printer::JUSTIFY_CENTER);
                     }
-                    #Judul
                     $printer -> text(str_repeat('-', $lebar_pixel)."\n");
-                    $batas = $lebar_pixel;
         
-                    #Item
                     $no = 0;
-                    $spasi_max_qty = 3;
-                    $spasi_between_qty_items = 1;
+                    /* Setting Qty */
+                    $max_qty = 3;
+                    $space_between_qty_item = 1;
+
+                    /* Setting Item */
+                    $max_item = $lebar_pixel - $max_qty - $space_between_qty_item;
+
+                    /* Setting Price */
+                    $max_price = 10;
+                    $space_between_price_sub_total = 2;
+
+                    /* Setting Sub Total */
+                    $max_sub_total = 10;
+
                     $printer -> setJustification(Printer::JUSTIFY_LEFT);
                     if($receipt->paper == "80mm"){
-                        $printer -> text("Qty Item".str_repeat(' ',13)."Price".str_repeat(' ',17)."Total\n");
+                        $printer -> text("Qty".str_repeat(' ',$space_between_qty_item)."Item".str_repeat(' ',23)."Price".str_repeat(' ',3)."Sub Total\n");
                     } else if($receipt->paper == "58mm"){
-                        $printer -> text("Qty Item".str_repeat(' ',7)."Price".str_repeat(' ',7)."Total\n");
+                        $printer -> text("Qty".str_repeat(' ',$space_between_qty_item)."Item".str_repeat(' ',7)."Price".str_repeat(' ',3)."Sub Total\n");
                     }
                     $printer -> text(str_repeat('-', $lebar_pixel)."\n");
                     foreach($receipt->contents as $content){
                     $no++;
-                        $nama_produk = ucwords(strtolower($content->name));#12
+                        $item = substr(ucwords(strtolower($content->name)),0,$lebar_pixel - $max_qty - $space_between_qty_item);#12
                         $qty = $content->qty;#1
                         $note = $content->note;
-                        $harga = uang($content->price);#6
+                        $price = uang($content->price);#6
                         $sub_total = uang($total[] = $content->qty*$content->price);#6
 
-                        $panjang_tengah = strlen($harga);
-                        $sisa_batas = $batas - $panjang_tengah;
-                        $sisa_batas_kiri = floor($sisa_batas/2);
-                        $sisa_batas_kanan = ceil($sisa_batas/2) - strlen($sub_total);
+                        $space_before_price = 26;
+                        if($receipt->paper == "58mm") {  $space_before_price = 10; }
                         
                         $printer -> setJustification(Printer::JUSTIFY_LEFT);
-                        $printer -> text(str_repeat(' ',$spasi_max_qty - strlen($qty)).$qty.str_repeat(' ',$spasi_between_qty_items).$nama_produk."\n");
-                        if($note != "" && $note != null){
-                            $printer -> text("     **".$note."\n");
+                        $printer -> text(str_repeat(' ',$max_qty - strlen($qty)).$qty.str_repeat(' ',$space_between_qty_item).$item."\n");
+                        $printer -> text(str_repeat(' ',$space_before_price).str_repeat(' ',$max_price - strlen($price)).$price.str_repeat(' ',$space_between_price_sub_total).str_repeat(' ',$max_sub_total - strlen($sub_total)).$sub_total."\n");
                         }
-                        $printer -> text(str_repeat(' ', $sisa_batas_kiri).$harga.str_repeat(' ', $sisa_batas_kanan).$sub_total."\n");
-                        }
-                        $total = array_sum($total);
-                        
+                $total = array_sum($total);
+               // Items         
                     
-            $batas = 10;
-            $panjang_total = strlen(uang($total));
+
+            
+            $total_length = strlen(uang($total));
             
             if($data->customer->disc_number > 0){
                 $disc = "-".uang((int)$data->customer->disc_number);
@@ -200,26 +216,33 @@ $jumlah_print = $_POST['jumlah_print'];
                 $grand_total = $total;
             }
             
-            $panjang_discb = strlen($disc);
-            $panjang_grand = strlen(uang($grand_total));
+            $discb_length = strlen($disc);
+            $grand_length = strlen(uang($grand_total));
             $paid = (int)$data->customer->paid;
-            $panjang_paid = strlen(uang($paid));
+            $paid_length = strlen(uang($paid));
             $change = ((int)$data->customer->changed == 0) ? '-' : uang((int)$data->customer->changed);
-            $panjang_change = strlen($change);
+            $change_length = strlen($change);
             $payment = $data->customer->payment;
-            $panjang_payment = strlen($payment);
+            $payment_length = strlen($payment);
 
-
-            if($receipt->type == '80'){
-            $batas_kanan = 48 - $lebar_pixel;
-            } else { $batas_kanan = 0; }
             $printer -> text(str_repeat('-', $lebar_pixel)."\n");
-            $printer -> setJustification(Printer::JUSTIFY_RIGHT);
-            $printer -> text("DISC     : ".str_repeat(' ', $batas - $panjang_discb + 2).$disc.str_repeat(' ', $batas_kanan)."\n");
-            $printer -> text("TOTAL    : ".str_repeat(' ', $batas - $panjang_grand + 2).uang($grand_total).str_repeat(' ', $batas_kanan)."\n");
-            $printer -> text("BAYAR    : ".str_repeat(' ', $batas - $panjang_paid + 2).uang($paid).str_repeat(' ', $batas_kanan)."\n");
-            $printer -> text("KEMBALI  : ".str_repeat(' ', $batas - $panjang_change + 2).$change.str_repeat(' ', $batas_kanan)."\n");
-            $printer -> text("PAYMENT  : ".str_repeat(' ', $batas - $panjang_payment + 2).$payment.str_repeat(' ', $batas_kanan)."\n");
+            $printer -> setJustification(Printer::JUSTIFY_LEFT);
+
+            $space_before = 25;
+            if($receipt->paper == "58mm") {  $space_before = 9; }
+            $max_center = 8;
+            $max_right = 15;
+
+            $space_between = $lebar_pixel - 11 - $discb_length;
+            $printer -> text(str_repeat(' ',$space_before)."DISC".str_repeat(' ',$max_center - strlen("DISC:")).":".str_repeat(' ',$max_right - strlen($disc)).$disc."\n");
+            $space_total = $lebar_pixel - 11 - $grand_length;
+            $printer -> text(str_repeat(' ',$space_before)."TOTAL".str_repeat(' ',$max_center - strlen("TOTAL:")).":".str_repeat(' ',$max_right - strlen(uang($grand_total))).uang($grand_total)."\n");
+            $space_paid = $lebar_pixel - 11 - $paid_length;
+            $printer -> text(str_repeat(' ',$space_before)."BAYAR".str_repeat(' ',$max_center - strlen("BAYAR:")).":".str_repeat(' ',$max_right - strlen(uang($paid))).uang($paid)."\n");
+            $space_change = $lebar_pixel - 11 - $change_length;
+            $printer -> text(str_repeat(' ',$space_before)."KEMBALI".str_repeat(' ',$max_center - strlen("KEMBALI:")).":".str_repeat(' ',$max_right - strlen($change)).$change."\n");
+            $space_payment = $lebar_pixel - 11 - $payment_length;
+            $printer -> text(str_repeat(' ',$space_before)."PAYMENT".str_repeat(' ',$max_center - strlen("PAYMENT:")).":".str_repeat(' ',$max_right - strlen($payment)).$payment."\n");
             $printer -> setJustification(Printer::JUSTIFY_LEFT);
             $printer -> text(str_repeat('=', $lebar_pixel)."\n");
             $printer -> setJustification(Printer::JUSTIFY_LEFT);
